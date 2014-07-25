106bdc14b69197afbbeb7d56352f78ac5b3c63d7
chaoKing <786773088@qq.com>
2014-07-25 11:33:54 +0800

    看是否有冲突

--- src/MyWelcome.java
+++ src/MyWelcome.java
@@ -1,6 +1,6 @@
 
 public class MyWelcome {
     public static void main(String args[]){
-        System.out.println("Welcome to javasky");
+        System.out.println("Welcome to javasky£¿£¿");
     }
 }
